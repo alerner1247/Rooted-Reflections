1ab4bae50ef80bb91efaa306ac14072b08c4b7f5
priscilla <plu@exeter.edu>
2025-11-04 12:30:43 -0500

    add initial files

---
+++ outline.py
@@ -0,0 +1,18 @@
+# Welcome
+
+# Here we want a pygame screen that says welcome, and a button that takes us to the next page
+
+# Choose if you want to input data or view garden
+# Four buttons, one that takes you to our input data screen, one that takes you to the garden, 
+# one that takes you to past gardens, one that takes you to music
+
+
+# Input data and transfer data to garden
+# You should only be able to input data once each day
+
+
+# View garden
+# Get text entry to appear when plant is clicked
+
+
+# Music to play, different songs the user can choose which one to play
