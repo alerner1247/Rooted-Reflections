a6bb9537f4849557dc19b4bab360e3dc2ef00ae9
priscilla <plu@exeter.edu>
2025-11-14 09:25:16 -0500

    music

--- music.py
+++ music.py
@@ -1,18 +1,45 @@
 import pygame
-import sys
-
 pygame.init()
-pygame.mixer.init()
 
-# Load and start music
-pygame.mixer.music.load("lofi.mp3")
-pygame.mixer.music.play(-1)
+# screen setup
+WIDTH, HEIGHT = 500, 400
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Button Music Player")
 
-while True:
+
+white = (255, 255, 255)
+blue = (100, 150, 255)
+
+#buttonbasicstepup
+button_rect = pygame.Rect(175, 150, 150, 60)  # x, y, width, height
+font = pygame.font.Font(None, 40)
+
+
+pygame.mixer.music.load("/Users/priscillalu/Downloads/lofi.mp3")
+
+running = True
+while running:
+    screen.fill(white)
+
+    # daw button
+    pygame.draw.rect(screen, blue, button_rect, border_radius=10)
+
+    # draw text
+    text = font.render("Play Music", True, white)
+    text_rect = text.get_rect(center=button_rect.center)
+    screen.blit(text, text_rect)
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.mixer.music.stop()
-            pygame.quit()
-            sys.exit()
+            running = False
+
+        # check mouse click
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if button_rect.collidepoint(event.pos):
+                pygame.mixer.music.play()   #start music!!!
 
     pygame.display.flip()
+
+pygame.quit()
+
